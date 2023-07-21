51dfbf5c446cdc4cf1821655f5d7c9d3787cf35f
MP <maxsu4@gmail.com>
2023-07-21 08:57:15 +0000

    Update 5 files

--- src/AreaChart.tsx
+++ src/AreaChart.tsx
@@ -1,31 +1,55 @@
-import React from 'react';
-import { Group } from '@visx/group';
-import { AreaClosed } from '@visx/shape';
-import { AxisLeft, AxisBottom, AxisScale } from '@visx/axis';
-import { LinearGradient } from '@visx/gradient';
-import { curveMonotoneX } from '@visx/curve';
-import { AppleStock } from '@visx/mock-data/lib/mocks/appleStock';
+import React from "react";
+import { Group } from "@visx/group";
+import { AreaClosed } from "@visx/shape";
+import { AxisLeft, AxisBottom, AxisScale } from "@visx/axis";
+import { LinearGradient } from "@visx/gradient";
+import { curveMonotoneX } from "@visx/curve";
+import { AppleStock } from "@visx/mock-data/lib/mocks/appleStock";
+import { DefaultNode, Graph } from "@visx/network";
+import {
+  Glyph as CustomGlyph,
+  GlyphCircle,
+  GlyphCross,
+  GlyphDiamond,
+  GlyphSquare,
+  GlyphStar,
+  GlyphTriangle,
+  GlyphWye,
+} from "@visx/glyph";
+import { appleStock } from "@visx/mock-data";
 
 // Initialize some variables
-const axisColor = '#fff';
+const axisColor = "#fff";
 const axisBottomTickLabelProps = {
-  textAnchor: 'middle' as const,
-  fontFamily: 'Arial',
+  textAnchor: "middle" as const,
+  fontFamily: "Arial",
   fontSize: 10,
   fill: axisColor,
 };
 const axisLeftTickLabelProps = {
-  dx: '-0.25em',
-  dy: '0.25em',
-  fontFamily: 'Arial',
+  dx: "-0.25em",
+  dy: "0.25em",
+  fontFamily: "Arial",
   fontSize: 10,
-  textAnchor: 'end' as const,
+  textAnchor: "end" as const,
   fill: axisColor,
 };
 
 // accessors
 const getDate = (d: AppleStock) => new Date(d.date);
 const getStockValue = (d: AppleStock) => d.close;
+
+interface CustomNode {
+  x: number;
+  y: number;
+  color?: string;
+}
+
+interface CustomLink {
+  source: CustomNode;
+  target: CustomNode;
+  dashed?: boolean;
+}
 
 export default function AreaChart({
   data,
@@ -55,6 +79,39 @@ export default function AreaChart({
   children?: React.ReactNode;
 }) {
   if (width < 10) return null;
+
+  type Positions = AppleStock & {
+    parent: string;
+  };
+
+  const positionsData: Positions[] = data.map((d: AppleStock, i) => {
+    return {
+      date: d.date,
+      close: d.close,
+      parent: data[Math.floor(Math.random() * data.length)].date,
+    };
+  });
+
+  const getX = (d: Positions) => xScale(getDate(d)) ?? 0;
+  const getY = (d: Positions) => yScale(getStockValue(d)) ?? 0;
+
+  const nodes: CustomNode[] = data.map((d: Positions, i) => {
+    return { x: getX(d), y: getY(d), color: "#26deb0" };
+  });
+
+  const links: CustomLink[] = nodes.map((d: CustomNode, i) => {
+    return {
+      source: d,
+      target: nodes[i < nodes.length - 1 ? i + 1 : i],
+      dashed: true,
+    };
+  });
+
+  const graph = {
+    nodes,
+    links,
+  };
+
   return (
     <Group left={left || margin.left} top={top || margin.top}>
       <LinearGradient
@@ -64,8 +121,8 @@ export default function AreaChart({
         to={gradientColor}
         toOpacity={0.2}
       />
-      <AreaClosed<AppleStock>
-        data={data}
+      <AreaClosed<Positions>
+        data={positionsData}
         x={(d) => xScale(getDate(d)) || 0}
         y={(d) => yScale(getStockValue(d)) || 0}
         yScale={yScale}
@@ -73,6 +130,26 @@ export default function AreaChart({
         stroke="url(#gradient)"
         fill="url(#gradient)"
         curve={curveMonotoneX}
+      />
+      <Graph<CustomLink, CustomNode>
+        graph={graph}
+        // top={20}
+        // left={100}
+        nodeComponent={({ node: { color } }) =>
+          color ? <DefaultNode fill={color} /> : <DefaultNode />
+        }
+        linkComponent={({ link: { source, target, dashed } }) => (
+          <line
+            x1={source.x}
+            y1={source.y}
+            x2={target.x}
+            y2={target.y}
+            strokeWidth={2}
+            stroke="#999"
+            strokeOpacity={0.6}
+            strokeDasharray={dashed ? "8,4" : undefined}
+          />
+        )}
       />
       {!hideBottomAxis && (
         <AxisBottom
